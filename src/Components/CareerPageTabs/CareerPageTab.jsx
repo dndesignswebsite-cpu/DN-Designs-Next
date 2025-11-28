71037e97d300ad42d3e5ba61c13c28d9807de9d8
Preet01001 <sharmapunit145@gmail.com>
2025-11-28 18:27:35 +0530

    chanfge

--- src/Components/CareerPageTabs/CareerPageTab.jsx
+++ src/Components/CareerPageTabs/CareerPageTab.jsx
@@ -1,20 +1,82 @@
-"use client"
+"use client";
 
-import React from 'react'
-import "./CareerPageTab.css"
+import React from "react";
+import "./CareerPageTab.css";
 
 function CareerPageTab() {
-    let tabsTittle =[1, 2, 3, 4, 5];
+  
 
   return (
     <div>
-          {
-            tabsTittle.map((item)=>{
-             return <h2>{item[3]}</h2>
-            })
-          }
+      <ul class="nav nav-tabs" role="tablist">
+        <li class="nav-item" role="presentation">
+          <a
+            class="nav-link active"
+            id="simple-tab-0"
+            data-bs-toggle="tab"
+            href="#simple-tabpanel-0"
+            role="tab"
+            aria-controls="simple-tabpanel-0"
+            aria-selected="true"
+          >
+            Tab 1
+          </a>
+        </li>
+        <li class="nav-item" role="presentation">
+          <a
+            class="nav-link"
+            id="simple-tab-1"
+            data-bs-toggle="tab"
+            href="#simple-tabpanel-1"
+            role="tab"
+            aria-controls="simple-tabpanel-1"
+            aria-selected="false"
+          >
+            Tab 2
+          </a>
+        </li>
+        <li class="nav-item" role="presentation">
+          <a
+            class="nav-link"
+            id="simple-tab-2"
+            data-bs-toggle="tab"
+            href="#simple-tabpanel-2"
+            role="tab"
+            aria-controls="simple-tabpanel-2"
+            aria-selected="false"
+          >
+            Tab 3
+          </a>
+        </li>
+      </ul>
+      <div class="tab-content pt-5" id="tab-content">
+        <div
+          class="tab-pane active"
+          id="simple-tabpanel-0"
+          role="tabpanel"
+          aria-labelledby="simple-tab-0"
+        >
+          Tab 1 selected
+        </div>
+        <div
+          class="tab-pane"
+          id="simple-tabpanel-1"
+          role="tabpanel"
+          aria-labelledby="simple-tab-1"
+        >
+          Tab 2 selected
+        </div>
+        <div
+          class="tab-pane"
+          id="simple-tabpanel-2"
+          role="tabpanel"
+          aria-labelledby="simple-tab-2"
+        >
+          Tab 3 selected
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CareerPageTab
+export default CareerPageTab;
