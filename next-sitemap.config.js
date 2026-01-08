43b6fdea996600a80b289b9ca45060143ab8efc1
Preet01001 <sharmapunit145@gmail.com>
2026-01-08 21:47:55 +0530

    Merged main into working

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://dn-designs-next.vercel.app',
-  generateRobotsTxt: true, // optional
-  sitemapSize: 7000, // if large website
-  changefreq: 'daily',
-  priority: 0.8,
-};
