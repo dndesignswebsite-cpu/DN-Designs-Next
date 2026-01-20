273266610065970ce5dc405f534fd91657335d64
Preet01001 <sharmapunit145@gmail.com>
2026-01-20 11:55:23 +0530

    maintance page remove

--- src/app/(main)/maintenance/page.js
+++
@@ -1,9 +0,0 @@
-// export default function MaintenancePage() {
-//   return (
-//     <div style={{ textAlign: 'center', marginTop: '20vh' }}>
-//       <h1>🚧 Website Under Maintenance</h1>
-//       <p>We are updating the website. Please check back soon.</p>
-//     </div>
-//   )
-// }
-  
