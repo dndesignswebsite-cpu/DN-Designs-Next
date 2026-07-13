2d6792a520f3e451f3cddf1abe9e34e9bafcd6f4
Preet01001 <sharmapunit145@gmail.com>
2026-07-13 14:11:49 +0530

    pop images

---
+++ src/Components/LPPackgingAgencyPopImages/LPPackgingAgencyPopImages.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./LPPackgingAgencyPopImages.css"
+
+function LPPackgingAgencyPopImages() {
+  return (
+    <div>
+     <div className="LPPackgingAgencyPopImagessection">
+        <img src="https://dndesigns.co.in/uploads/videos/lppopupimageMAKIO.png" className='LPPackgingAgencyPopImagessectionimg1 img-fluid'/>
+
+        <img src="https://dndesigns.co.in/uploads/videos/lppopupimage5.png" className='LPPackgingAgencyPopImagessectionimg2 img-fluid'/>
+
+        <img src="https://dndesigns.co.in/uploads/videos/lppopupimage4(1).png" className='LPPackgingAgencyPopImagessectionimg3 img-fluid'/>
+
+        <img src="https://dndesigns.co.in/uploads/videos/lppopupimage3(1).png" className='LPPackgingAgencyPopImagessectionimg4 img-fluid'/>
+
+        <img src="https://dndesigns.co.in/uploads/videos/lppopupimage2(1).png" className='LPPackgingAgencyPopImagessectionimg5 img-fluid'/>
+     </div>
+    </div>
+  )
+}
+
+export default LPPackgingAgencyPopImages
